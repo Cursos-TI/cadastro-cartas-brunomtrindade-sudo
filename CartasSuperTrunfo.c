97ec08c6918da9a136ae82348c39a7bda1d6f309
Bruno Trindade <270747136+brunomtrindade-sudo@users.noreply.github.com>
2026-03-25 23:30:45 +0000

    Fiz o desafio Super Trunfo nível novato. Todo o código foi escrito pelo Code Blocks.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,67 @@
 #include <stdio.h>
-
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
-
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+    int populacao, populacao2;
+    int turismo, turismo2;
+    float area, area2;
+    float pib, pib2;
+    char codigo[3], codigo2[3];
+    char letra, letra2;
+    char nome[30], nome2[30];
 
-  // Área para entrada de dados
+        //Parâmetros para a primeira carta:
 
-  // Área para exibição dos dados da cidade
+    printf("Aqui você irá inserir os dados do estado para a primeira carta. \n");
+    printf("Nome do estado: \n");
+    fgets(nome, 30, stdin);
 
-return 0;
-} 
+    printf("Letra do estado, de 'A' a 'H': \n");
+    scanf(" %c", &letra);
+
+    printf("Área do estado: \n");
+    scanf("%f", &area);
+
+    printf("Número de pontos turísticos: \n");
+    scanf("%d", &turismo);
+
+    printf("População do estado: \n");
+    scanf("%d", &populacao);
+
+    printf("PIB do estado: \n");
+    scanf("%f", &pib);
+
+    printf("Código do estado: \n");
+    scanf("%s", &codigo);
+    getchar();
+
+
+        //Parâmetros para a segunda carta.
+
+    printf("Agora, insera os dados do estado para a segunda carta. \n");
+    printf("Nome do estado: \n");
+    fgets(nome2, 30, stdin);
+
+    printf("Letra do estado, de 'A' a 'H': \n");
+    scanf(" %c", &letra2);
+
+    printf("Área do estado: \n  ");
+    scanf("%f", &area2);
+
+    printf("Número de pontos turísticos: \n");
+    scanf("%d", &turismo2);
+
+    printf("População do estado: \n");
+    scanf("%d", &populacao2);
+
+    printf("PIB do estado: \n");
+    scanf("%f", &pib2);
+
+    printf("Código do estado: \n");
+    scanf(" %s", &codigo2);
+
+        //Comparação entre as cartas:
+    printf("Comparação entre as cartas: \n");
+    printf("Carta 1:\n Nome: %s -Letra: %c \n-Área: %.2f Km²\n-Pontos Turísticos: %d \n-População: %d \n-PIB: %f Bilhões de Reais \n-Código da carta: %s \n", nome, letra, area, turismo, populacao, pib, codigo);
+    printf("Carta 2:\n Nome: %s -Letra: %c \n-Área %.2f Km²\n-Pontos Turísticos: %d \n-População: %d \n-PIB: %f Bilhões de Reais \n-Código da carta: %s \n", nome2, letra2, area2, turismo2, populacao2, pib2, codigo2);
+
+    return 0;
+}
