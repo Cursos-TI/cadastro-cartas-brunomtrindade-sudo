94feacd65b09c7bb636a99c10df701a17b91e2b0
Bruno Trindade <270747136+brunomtrindade-sudo@users.noreply.github.com>
2026-03-30 15:14:01 -0300

    Desafio Mestre
    Alteração de população de int para Unsigned Long Int e adicionado as funções descritas no "Desafio Mestre"

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int populacao, populacao2;
+    unsigned long int populacao, populacao2;
     int turismo, turismo2;
     float area, area2, densidade, densidade2;
     float pib, pib2, percapita, percapita2;
-    char codigo[5], codigo2[5];
+    float trunfo, trunfo2;
+    char codigo[3], codigo2[3];
     char letra, letra2;
     char nome[30], nome2[30];
+    
 
     //Parâmetros para a primeira carta:
 
@@ -25,22 +27,22 @@ int main() {
     scanf("%d", &turismo);
 
     printf("População do estado: \n");
-    scanf("%d", &populacao);
+    scanf("%lu", &populacao);
 
     printf("PIB do estado: \n");
     scanf("%f", &pib);
-    percapita = (float)pib / populacao;
+    percapita = pib / populacao;
 
     printf("Código do estado: \n    ");
     scanf("%s", codigo);
 
-    densidade = (float)populacao / area;
+    densidade = populacao / area;
 
     //Parâmetros para a segunda carta.
 
     printf("Agora, insira os dados do estado para a segunda carta. \n");
     printf("Nome do estado: \n");
-    getchar(); // limpa o buffer antes do fgets
+    getchar();
     fgets(nome2, 30, stdin);
 
     printf("Letra do estado, de 'A' a 'H': \n");
@@ -53,12 +55,12 @@ int main() {
     scanf("%d", &turismo2);
 
     printf("População do estado: \n");
-    scanf("%d", &populacao2);
-    densidade2 = (float)populacao2 / area2;
+    scanf("%lu", &populacao2);
+    densidade2 = populacao2 / area2;
 
     printf("PIB do estado: \n");
     scanf("%f", &pib2);
-    percapita2 = (float)pib2 / populacao2;
+    percapita2 = pib2 / populacao2;
 
     printf("Código do estado: \n");
     scanf("%s", codigo2);
@@ -70,24 +72,43 @@ int main() {
     printf("Carta 1:\n");
 printf("Nome: %s", nome);
 printf("Letra: %c\n", letra);
-printf("Área: %f Km²\n", area);
+printf("Área: %.2f Km²\n ", area);
 printf("Pontos turísticos: %d\n", turismo);
-printf("População: %d\n", populacao);
-printf("Densidade Populacional: %f Hab/km²\n", densidade);
-printf("PIB: %f Trilhões de reais\n", pib);
-printf("PIB per capita: %.1f\n", percapita);
-printf("Código: %s\n", codigo);
+printf("População: %lu\n", populacao);
+printf("Densidade Populacional: %.2f Hab/km²\n", densidade);
+printf("PIB: %.2f Trilhões de reais\n", pib);
+printf("PIB per capita: %.2f\n", percapita);
+printf("Código: %s\n\n", codigo);
 
 printf("Carta 2:\n");
 printf("Nome: %s", nome2);
 printf("Letra: %c\n", letra2);
-printf("Área: %f Km²\n", area2);
+printf("Área: %.2f Km²\n", area2);
 printf("Pontos turísticos: %d\n", turismo2);
-printf("População: %d\n", populacao2);
-printf("Densidade Populacional: %f Hab/km²\n", densidade2);
-printf("PIB: %f Trilhões de reais\n", pib2);
-printf("PIB per capita: %f Reais\n", percapita2);
+printf("População: %lu\n", populacao2);
+printf("Densidade Populacional: %.2f Hab/km²\n", densidade2);
+printf("PIB: %.2f Trilhões de reais\n", pib2);
+printf("PIB per capita: %.2f Reais\n", percapita2);
 printf("Código: %s\n", codigo2);
+
+    //Declaração de vencedor:
+        printf("O vencedor é: \n");
+    trunfo = (area + turismo + densidade + pib + percapita) / 5;
+    trunfo2 = (area2 + turismo2 + densidade2 + pib2 + percapita2) / 5;
+    
+
+    populacao > populacao2 ? printf("População: %s venceu!\n", nome) : printf("População: %s venceu!\n", nome2);
+    area > area2 ? printf("Área: %s venceu!\n", nome) : printf("Área: %s venceu!\n", nome2);
+    turismo > turismo2 ? printf("Turismo: %s venceu!\n", nome) : printf("Turismo: %s venceu!\n", nome2);
+    densidade < densidade2 ? printf("Densidade: %s venceu!\n", nome) : printf("Densidade: %s venceu!\n", nome2);
+    pib > pib2 ? printf("PIB: %s venceu!\n", nome) : printf("PIB: %s venceu!\n", nome2);
+    percapita > percapita2 ? printf("PIB per capita: %s venceu!\n", nome) : printf("PIB per capita: %s venceu!\n", nome2);
+    trunfo > trunfo2 ? printf("Super Poder: %s venceu!\n", nome) : printf("Super Poder: %s venceu!\n", nome2);
+
+
+
+    
+
 
     return 0;
 }
